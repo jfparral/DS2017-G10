830cbfe3b8654406af5d886d57ebb8de5dc16816
Jonathan <jparrales15@gmail.com>
2017-08-15 14:38:37 -0500

    Agregando Menus sin implementar

---
+++ SegundoParcial/src/segundoparcial/Salir.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segundoparcial;
+
+/**
+ *
+ * @author usuario
+ */
+public class Salir implements MenuDecorator{
+
+    @Override
+    public void AgregarMenu(User usuario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Implementar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
