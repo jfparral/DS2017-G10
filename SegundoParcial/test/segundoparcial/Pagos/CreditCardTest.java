49b8f06c5dfd181ffb3ebe712fa28a69aed31ed1
daniloti1 <danilo.ti@hotmail.com>
2017-08-21 23:31:33 -0500

    ultimo de testing

--- SegundoParcial/test/segundoparcial/Pagos/CreditCardTest.java
+++ SegundoParcial/test/segundoparcial/Pagos/CreditCardTest.java
@@ -8,6 +8,9 @@ package segundoparcial.Pagos;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import segundoparcial.Personas.Cliente;
+import segundoparcial.Platillos.Ejecutivo;
+import segundoparcial.Platillos.Estudiantil;
+import segundoparcial.Platillos.Platillo;
 
 /**
  *
@@ -41,4 +44,58 @@ public class CreditCardTest {
         instance.RealizarPago(costo);
     }
     
+    @Test
+    public void testCompra1() {
+        System.out.println("Compra1");
+        Platillo platillo = new Estudiantil("nombre", "descripcion", "categoria", "teperatura", "tipo", null);
+        platillo.PrecioPlatillo();        
+        CreditCard instance = new CreditCard(new Cliente("usuario", "contrasena"), 50);
+        instance.RealizarPago(platillo.getPrecio());
+        CreditCard instance2 = new CreditCard(new Cliente("usuario", "contrasena"), 1);
+        instance2.RealizarPago(platillo.getPrecio());        
+    }
+    
+    @Test
+    public void testCompra2() {
+        System.out.println("Compra2");
+        Platillo platillo = new Ejecutivo("nombre", "descripcion", "categoria", "teperatura", "tipo", null);
+        platillo.PrecioPlatillo();        
+        CreditCard instance = new CreditCard(new Cliente("usuario", "contrasena"), 50);
+        instance.RealizarPago(platillo.getPrecio());
+        CreditCard instance2 = new CreditCard(new Cliente("usuario", "contrasena"), 1);
+        instance2.RealizarPago(platillo.getPrecio());        
+    }
+    
+    @Test
+    public void testCompra3() {
+        System.out.println("Compra3");
+        Platillo platillo = new Ejecutivo("nombre", "descripcion", "categoria", "teperatura", "tipo", null);
+        platillo.setPrecio(3.5);
+        CreditCard instance = new CreditCard(new Cliente("usuario", "contrasena"), 50);
+        instance.RealizarPago(platillo.getPrecio());
+        CreditCard instance2 = new CreditCard(new Cliente("usuario", "contrasena"), 1);
+        instance2.RealizarPago(platillo.getPrecio());        
+    }
+    
+    @Test
+    public void testCompra4() {
+        System.out.println("Compra4");
+        Platillo platillo = new Ejecutivo("nombre", "descripcion", "categoria", "teperatura", "tipo", null);
+        platillo.setPrecio(3.75);
+        CreditCard instance = new CreditCard(new Cliente("usuario", "contrasena"), 50);
+        instance.RealizarPago(platillo.getPrecio());
+        CreditCard instance2 = new CreditCard(new Cliente("usuario", "contrasena"), 1);
+        instance2.RealizarPago(platillo.getPrecio());        
+    }
+    
+    @Test
+    public void testCompra5() {
+        System.out.println("Compra5");
+        Platillo platillo = new Ejecutivo("nombre", "descripcion", "categoria", "teperatura", "tipo", null);
+        platillo.setPrecio(4.25);
+        CreditCard instance = new CreditCard(new Cliente("usuario", "contrasena"), 50);
+        instance.RealizarPago(platillo.getPrecio());
+        CreditCard instance2 = new CreditCard(new Cliente("usuario", "contrasena"), 1);
+        instance2.RealizarPago(platillo.getPrecio());        
+    }
 }
